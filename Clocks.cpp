595f361add6f6fa9b5667a740d95d251ea8f1ef7
MJ Belleville <villebellez@outlook.com>
2024-01-19 23:07:23 -0500

    Update Clocks.cpp
    Added user input time and validation

--- Clocks.cpp
+++ Clocks.cpp
@@ -4,10 +4,14 @@
 #include <string>
 #include <iomanip>
 #include <sstream>
+#include <stdexcept>
 using namespace std;
+
+void validateTime(string hours, string minutes, string seconds);
 
 void displayMenu() {
 
+    // TODO: Make functional
     cout << endl;
     cout << "MENU OPTIONS:" << endl;
     cout << setfill('*') << setw(26) << "" << endl;
@@ -21,6 +25,7 @@ void displayMenu() {
 
 void displayClock(string hour24 = "00", string hour12 = "12", string min = "00", string sec = "00", string ampm = "AM") {
    
+    // Testing Display
     cout << setfill('*') << setw(26) << "";
     cout << setfill(' ') << setw(10) << "";
     cout << setfill('*') << setw(26) << "" << endl;
@@ -37,9 +42,9 @@ void displayClock(string hour24 = "00", string hour12 = "12", string min = "00",
     displayMenu();
 }
 
-int main() {
-    string userTime;
+void inputTime() {
     string hours, minutes, seconds;
+    string userTime;
 
     cout << "Please enter the initial time in 24-Hour format (hour/min/sec - 00:00:00)" << endl;
     getline(cin, userTime);
@@ -49,10 +54,39 @@ int main() {
     getline(iss, minutes, ':');
     getline(iss, seconds, ':');
 
-    // Testing
-    cout << "Hours: " << hours << endl;
-    cout << "Minutes: " << minutes << endl;
-    cout << "Seconds: " << seconds << endl;
+    validateTime(hours, minutes, seconds);
+
+}
+
+void validateTime(string hours, string minutes, string seconds) {
+    string userTime;
+    string time[] = { hours, minutes, seconds };
+
+    for (int i = 0; i < 3; i++) {
+        if (time[i][0] == '0') {
+            time[i] = time[i].substr(1);
+        }
+    }
+
+    int hrs = stoi(time[0]);
+    int mins = stoi(time[1]);
+    int secs = stoi(time[2]);
+
+    try {
+        if (hrs > 23 || mins > 59 || secs > 59) {
+            throw invalid_argument("Invalid time format, please try again.\n");
+        }
+    }
+    catch (invalid_argument& e) {
+        cout << e.what() << endl;
+        inputTime();
+    }
+}
+
+
+int main() {
+
+    inputTime();
 
     displayClock();
 
